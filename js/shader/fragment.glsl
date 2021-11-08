51f2f5eae5dc3f1b492807043bbf4be8e01d3d86
mgdomingues <mdomingues@campus.ul.pt>
2021-11-08 23:45:16 +0000

    applying refraction to every face based on the diffuse value, implement a eyeVector as a point of view

--- js/shader/fragment.glsl
+++ js/shader/fragment.glsl
@@ -4,17 +4,23 @@ uniform sampler2D landscape;
 uniform vec4 resolution;
 varying vec2 vUv;
 varying vec3 vNormal;
+
+varying vec3 eyeVector;
+
 float PI = 3.141592653589793238;
 void main() {
 
-  vec2 uv = vec2(0.5,-0.5)*gl_FragCoord.xy/vec2(1000.);
+  vec2 uv = gl_FragCoord.xy/vec2(1000.);
 
 	vec3 X = dFdx(vNormal);
 	vec3 Y = dFdy(vNormal);
 	vec3 normal = normalize(cross(X,Y));
 
-  float diffuse = dot(normal,vec3(1.));
-	vec4 t = texture2D(landscape, uv);
+  vec3 refracted = refract(eyeVector,normal,1./3.);
+	uv += refracted.xy;
+
+	float diffuse = dot(normal,vec3(1.));
+	vec4 t = texture2D(landscape,uv);
   //gl_FragColor = vec4(vUv,0.0,1.);
   gl_FragColor = t;
   //gl_FragColor = vec4(diffuse);
